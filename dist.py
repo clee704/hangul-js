0ee6231eb6981275ecf7ae334ba3fc989f824e8f
Choongmin Lee <choongmin88@gmail.com>
2011-10-28 14:12:50 +0900

    made the build system handle multiple targets

--- dist.py
+++ dist.py
@@ -1,32 +1,37 @@
 #! /usr/bin/env python
-import glob, os, sys, subprocess, tempfile
+import glob, os, re, sys, subprocess, tempfile
 import yaml
 
 def main(argv):
     retval = 0
     manifest = yaml.load(open('manifest.yaml'))
-    print 'Collecting files from {0}'.format(manifest['src_dir'])
-    contents = combine(manifest['src_dir'], manifest['combine_order'])
-    print 'Replacing @VERSION with {0}'.format(manifest['version'])
-    contents = contents.replace('@VERSION', manifest['version'])
-    temp = tempfile.NamedTemporaryFile(suffix='.js', delete=False)
-    temp.write(contents)
-    temp.close()
-    dist_dir = manifest['dist_dir']
-    if not os.path.isdir(dist_dir):
-        print 'Creating a new directory at {0}'.format(dist_dir)
-        os.mkdir(dist_dir)
-    target_path = os.path.join(dist_dir, manifest['combine_name'])
-    print 'Compressing with {0}'.format(manifest['yuicompressor'])
-    if 0 == compress(manifest['yuicompressor'], temp.name, target_path):
-        print 'Compressed data is written at {0}'.format(target_path)
-    else:
-        print 'Compression failed'
-        retval = 1
-    os.remove(temp.name)
-    sys.exit(retval)
+    for target_name, input_files in manifest['targets'].iteritems():
+        log('Target: {0}\n'.format(target_name))
+        contents = combine(manifest['src_dir'], input_files)
+        log('  * Replacing @VERSION with {0}\n'.format(manifest['version']))
+        contents = contents.replace('@VERSION', manifest['version'])
+        temp = tempfile.NamedTemporaryFile(suffix='.js', delete=False)
+        temp.write(contents)
+        temp.close()
+        dist_dir = manifest['dist_dir']
+        if not os.path.isdir(dist_dir):
+            log('  * Creating a new directory at {0}\n'.format(dist_dir))
+            os.mkdir(dist_dir)
+        target_path = os.path.join(dist_dir, target_name)
+        with open(target_path, 'w') as f:
+            log('  * Writing uncompress data at {0}\n'.format(target_path))
+            f.write(contents)
+        target_path_min = re.sub('\.js$', '.min.js', target_path)
+        retval = compress(manifest['yuicompressor'], temp.name, target_path_min)
+        os.remove(temp.name)
+        if retval:
+            log('  * Compression failed\n')
+            sys.exit(retval)
+        else:
+            log('  * Compressed data is written at {0}\n'.format(target_path_min))
 
 def combine(src_dir, combine_order):
+    log('  * Collecting files from {0}\n'.format(src_dir))
     paths = []
     for p in (glob.glob(os.path.join(src_dir, fn)) for fn in combine_order):
         paths.extend(p)
@@ -37,12 +42,17 @@ def combine(src_dir, combine_order):
             continue
         visited.add(p)
         with open(p) as f:
+            log('    - Reading {0}\n'.format(p))
             contents.append(f.read())
     return ''.join(contents)
 
-def compress(yuicompressor, input, output):
-    args = ['java', '-jar', yuicompressor, '-o', output, input]
+def compress(yuicompressor, input_path, output_path):
+    args = ['java', '-jar', yuicompressor, '-o', output_path, input_path]
     return subprocess.call(args)
+
+def log(message):
+    sys.stdout.write(message)
+    sys.stdout.flush()
 
 if __name__ == '__main__':
     main(sys.argv)
